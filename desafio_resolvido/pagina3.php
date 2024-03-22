2d43585f7d6e612d05ac68a9fba37f7b05cdd23f
almeida-cma <93884913+almeida-cma@users.noreply.github.com>
2024-03-22 11:41:35 -0300

    Update pagina3.php

--- desafio_resolvido/pagina3.php
+++ desafio_resolvido/pagina3.php
@@ -1,19 +1,8 @@
 <?php
 session_start();
 
-$nome1 = $_SESSION["nome"];
-$idade1 = $_SESSION["idade"];
-$peso1 = $_SESSION["peso"];
-$altura1 = $_SESSION["altura"];
+//Código aqui
 
-$nome2 = $_SESSION["nome2"];
-$idade2 = $_SESSION["idade2"];
-$peso2 = $_SESSION["peso2"];
-$altura2 = $_SESSION["altura2"];
-
-$maiorAltura = ($altura1 > $altura2) ? $nome1 : $nome2;
-$maisVelho = ($idade1 > $idade2) ? $nome1 : $nome2;
-$maiorPeso = ($peso1 > $peso2) ? $nome1 : $nome2;
 ?>
 
 <!DOCTYPE html>
